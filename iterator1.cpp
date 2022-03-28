eecda8844e00439c4338fcaa8a5fbd970fd12d20
おっぱいは愛 <kengsini250@gmail.com>
2022-03-28 21:41:56 +0800

    Add files via upload

---
+++ iterator1.cpp
@@ -0,0 +1,87 @@
+﻿#include <iostream>
+
+using namespace std;
+
+namespace Test
+{
+#define MAX 10
+
+	class Iterator
+	{
+	public:
+		int* index = nullptr;
+
+		Iterator(int* arr)
+		{
+			index = arr;
+		}
+
+
+		int operator*()const
+		{
+			return *index;
+		}
+
+		Iterator& operator++(int)
+		{
+			++index;
+			return *this;
+		}
+
+		bool operator!=(const Iterator&right)const
+		{
+			return index != right.index;
+		}
+
+		Iterator& operator-(int)
+		{
+			--index;
+			return *this;
+		}
+	};
+
+
+	class List
+	{
+	public:
+		using Type = int;
+
+	private:
+		Type arr[MAX] = {0};
+	public:
+		List() 
+		{
+			for (int i = 0; i < MAX; i++) {
+				arr[i] = i;
+			}
+		}
+
+		Iterator begin()
+		{
+			return Iterator(arr);
+		}
+
+		Iterator end()
+		{
+			return Iterator(&arr[MAX]);
+		}
+
+	};
+
+}
+
+int main()
+{
+	Test::List list;
+	Test::Iterator p = list.begin();
+	cout << *p << endl;
+	p++;
+	cout << *p << endl;
+	p = list.end();
+	cout << *(p-1) << endl;
+
+	for (auto p = list.begin(); p != list.end(); p++) {
+		cout << *p << endl;
+	}
+}
+
